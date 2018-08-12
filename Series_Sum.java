1486761e67ae8d7f2428cde66ba417654b85e7a5
saGen <sagensakam015@gmail.com>
2018-08-12 15:04:44 +0530

    Program to calculate the Sum of the series .
    //1+2+3+4+5+6+--------+n

---
+++ Series_Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//Program to calculate the Sum of the series .
+//1+2+3+4+5+6+--------+n
+public class Series_Sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print(" Enter a range : ");
+        int n=sc.nextInt();
+        int res=(n*(n+1))/2;
+        System.out.println(res);
+    }
+}
